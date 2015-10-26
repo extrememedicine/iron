ce27bd11301ca08eae09ebc19c58b2699ee07e8a
James Bulmer <nekinie@gmail.com>
2015-10-26 15:12:25 +0000

    These elastic calls are really bad, pressed for time :/

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"text/template"
 	"time"
 
 	"github.com/bmizerany/pat"
-	"github.com/gocql/gocql"
 )
 
 var (
 	templates *template.Template
-	cluster   *gocql.ClusterConfig
 )
+
+type SMS struct {
+	ID        string
+	To        string
+	From      string
+	Body      string
+	Timestamp string
+}
 
 func nexmo(w http.ResponseWriter, r *http.Request) {
 
@@ -39,23 +48,20 @@ func nexmo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	session, err := cluster.CreateSession()
+	var jsonStr = []byte(`{"to":"` + r.Form.Get("to") + `","from":"` + r.Form.Get("msisdn") + `","body":"` + r.Form.Get("text") + `","timestamp":"` + timestamp.UTC().String() + `"}`)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:9200/messages/"+r.Form.Get("msisdn"), bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(req)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("ERROR"))
-		return
+		panic(err)
 	}
 
-	defer session.Close()
+	defer res.Body.Close()
 
-	err = session.Query(`INSERT INTO messages (id, receiver, sender, body, time) VALUES (?, ?, ?, ?, ?)`, gocql.TimeUUID(), r.Form.Get("to"), r.Form.Get("msisdn"), r.Form.Get("text"), timestamp.UTC()).Exec()
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("ERROR"))
-		return
-	}
+	body, _ := ioutil.ReadAll(res.Body)
+
+	log.Println(string(body))
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
@@ -64,10 +70,71 @@ func nexmo(w http.ResponseWriter, r *http.Request) {
 
 func dashboard(w http.ResponseWriter, r *http.Request) {
 
-	err := templates.ExecuteTemplate(w, "index.html", nil)
+	var jsonStr = []byte(`{"query":{"match_all":{}},"aggs":{"top-types":{"terms":{"field":"_type"},"aggs":{"top_docs":{"top_hits":{"sort":[{"_score":{"order":"desc"}}],"size":1}}}}}}`)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:9200/messages/_search?search_type=count", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	log.Println(string(body))
+
+	data := struct {
+		Aggregations struct {
+			Top struct {
+				Buckets []struct {
+					Top struct {
+						Hits struct {
+							Hits []struct {
+								Index  string `json:"_index"`
+								Type   string `json:"_type"`
+								ID     string `json:"_id"`
+								Source struct {
+									To        string `json:"to"`
+									From      string `json:"from"`
+									Body      string `json:"body"`
+									Timestamp string `json:"timestamp"`
+								} `json:"_source"`
+							} `json:"hits"`
+						} `json:"hits"`
+					} `json:"top_docs"`
+				} `json:"buckets"`
+			} `json:"top-types"`
+		} `json:"aggregations"`
+	}{}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	messages := []SMS{}
+
+	for _, bucket := range data.Aggregations.Top.Buckets {
+
+		messages = append(messages, SMS{
+			bucket.Top.Hits.Hits[0].ID,
+			bucket.Top.Hits.Hits[0].Source.To,
+			bucket.Top.Hits.Hits[0].Source.From,
+			bucket.Top.Hits.Hits[0].Source.Body,
+			bucket.Top.Hits.Hits[0].Source.Timestamp,
+		})
+
+	}
+
+	log.Println(messages)
+
+	err = templates.ExecuteTemplate(w, "index.html", struct{ Messages []SMS }{messages})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("ERROR"))
+		return
 	}
 
 }
@@ -80,9 +147,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	cluster = gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "iron"
 
 	router := pat.New()
 
